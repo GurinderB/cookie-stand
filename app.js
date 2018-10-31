fcf2daaea2e3b23ad1aaf89666b95128b9987b45
Gurinder Batth <gurinder@gmx.com>
2018-10-31 08:39:13 -0700

    First Commit for this branch

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-"use strict";
+//"use strict";
 
-//var hrs = ['6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00'];
+/*var hrs = ['6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00'];
 var shopPike = {
   name: '1\'st and Pike',
   minCust: 23,
@@ -25,4 +25,38 @@ var shopPike = {
   },
 
 };
+*/
+var shops = [];
+function CookStnd(name, minCust, maxCust, avgCookPerCust) {
+
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookPerCust = avgCookPerCust;
+  this.HrsOfOpp = ['6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00'];
+  this.custPerHr = [];
+  this.hourlySales = [];
+  this.dailySales = 0;
+  this.randC = function (min, max) {
+    for (var i = 0; i < this.HrsOfOpp.lenght; i++) {
+      var randCust = Math.floor(Math.random() * (max - min) + min);
+      this.custPerHr.push(randCust);
+
+    }
+  };
+  this.generateHourlySales = function () {
+    this.randC(this.minCust, this.maxCust);
+
+    for (var i = 0; i < this.hoursOfOps.length; i++) {
+      var perHour = Math.round(this.custPerHr[i] * this.avgCookPerCust);
+      this.hourlySales.push(perHour);
+
+      // this.dailyTotal = this.dailyTotal + perHour;
+      this.dailySales += perHour;
+    }
+  };
+  shops.push(this);
+}
+
+new CookStnd('Pike', 6, 65, 6.3);
 
